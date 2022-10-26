64e0534bef9402a18b26f60955ebf5e1bcc438bd
WookyungYun <gyong1540@gmail.com>
2022-10-26 14:03:14 +0900

    loading State 설정

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -4,7 +4,7 @@ import Seo from "../components/Seo";
 const API_KEY = "9ce74eddeae24b12485699a5f557b4d8";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState();
   useEffect(() => {
     (async () => {
       const { results } = await (
@@ -18,7 +18,8 @@ export default function Home() {
   return (
     <div>
       <Seo title="Home" />
-      {movies.map((movie) => (
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
         <div key={movie.id}>
           <h4>{movie.original_title}</h4>
         </div>
