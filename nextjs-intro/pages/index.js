ac5e14a353afbd94119371b9ede19b8acf1f1ecc
WookyungYun <gyong1540@gmail.com>
2022-10-26 13:58:27 +0900

    API연결

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
+
+const API_KEY = "9ce74eddeae24b12485699a5f557b4d8";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+        )
+      ).json();
+      setMovies(results);
+    })();
+  }, []);
   return (
     <div>
       <Seo title="Home" />
-      <h1>Hello</h1>
+      {movies.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 }
